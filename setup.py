c9f7408f1c75445322efd1bfc09ee25011d60d6b
Cong Ling <adonis0147@yahoo.com.cn>
2012-10-08 23:08:59 +0800

    Python 3.x supported

--- setup.py
+++ setup.py
@@ -2,16 +2,33 @@
 # -*- coding: utf-8 -*-
 
 from distutils.core import setup
-import yah3c.yah3c
+from platform import python_version
 
-setup(name='yah3c',
-      version=yah3c.yah3c.__version__,
-      description='A program for h3c authentication in SYSU east campus.',
-      author='maple',
-      author_email='maplevalley8@gmail.com',
-      url='https://github.com/humiaozuzu/YaH3C',
-      download_url='https://github.com/humiaozuzu/YaH3C',
-      license='MIT',
-      packages=['yah3c', 'yah3c/colorama'],
-      scripts=['scripts/yah3c'],
-      )
+if python_version().startswith('2'):
+    import yah3c.yah3c
+
+    setup(name='yah3c',
+          version=yah3c.yah3c.__version__,
+          description='A program for h3c authentication in SYSU east campus.',
+          author='maple',
+          author_email='maplevalley8@gmail.com',
+          url='https://github.com/humiaozuzu/YaH3C',
+          download_url='https://github.com/humiaozuzu/YaH3C',
+          license='MIT',
+          packages=['yah3c', 'yah3c/colorama'],
+          scripts=['scripts/yah3c'],
+          )
+else:
+    import yah3c_py3.yah3c
+
+    setup(name='yah3c',
+          version=yah3c_py3.yah3c.__version__,
+          description='A program for h3c authentication in SYSU east campus.',
+          author='maple',
+          author_email='maplevalley8@gmail.com',
+          url='https://github.com/humiaozuzu/YaH3C',
+          download_url='https://github.com/humiaozuzu/YaH3C',
+          license='MIT',
+          packages=['yah3c_py3', 'yah3c_py3/colorama'],
+          scripts=['scripts_py3/yah3c'],
+          )
